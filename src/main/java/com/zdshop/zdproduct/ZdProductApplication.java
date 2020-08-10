33acdcc04bf44130c819344ecb9fad36e2d9a7e6
Nawal <nawal@MBP-de-Nawal.home>
2020-08-10 22:53:22 +0200

    first commit

---
+++ src/main/java/com/zdshop/zdproduct/ZdProductApplication.java
@@ -0,0 +1,13 @@
+package com.zdshop.zdproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZdProductApplication.class, args);
+	}
+
+}
