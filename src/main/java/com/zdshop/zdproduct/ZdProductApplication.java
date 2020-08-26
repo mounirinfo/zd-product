1ba2e5a5a57ccb5b8487fe26b19b506afad21080
Mounir ZAGHBA <zaghba.mounir@gmail.com>
2020-08-26 21:48:33 +0200

    register to eureka

--- src/main/java/com/zdshop/zdproduct/ZdProductApplication.java
+++ src/main/java/com/zdshop/zdproduct/ZdProductApplication.java
@@ -2,8 +2,10 @@ package com.zdshop.zdproduct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ZdProductApplication {
 
 	public static void main(String[] args) {
