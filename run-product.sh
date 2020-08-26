ca899fb7d1902ba1aa5d3c154e33e685bae8951e
Aghilas DJEMA <aghilas@mbp-de-aghilas.home>
2020-08-26 21:10:14 +0200

    edit sh file

--- run-product.sh
+++ run-product.sh
@@ -1,4 +1,13 @@
+#start app on docker container
 
+#shoose java version
+export JAVA_HOME=`/usr/libexec/java_home -v 14`
 
-#docker build -t zd/zd-product:0.0.1 .
-#docker run --rm -d -p 9090:9090  zd/zd-product:0.0.1
+#build JAR file
+mvn clean install
+
+#build docker image
+docker build -t zd/zd-product:0.0.1 .
+
+#start docker container
+docker run --rm -d -p 9090:9090  zd/zd-product:0.0.1
